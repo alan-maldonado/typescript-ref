f16063c2d10214c67939eb50f62418052875eb1d
alan-maldonado <alan.maldonado@gmail.com>
2019-07-28 13:45:55 -0500

    ✨ Added csv reader generic

---
+++ stats/src/CsvReader.ts
@@ -0,0 +1,21 @@
+import fs from 'fs';
+import { MatchResult } from './MatchResult';
+import { dateFromString } from './utils';
+
+export abstract class CsvReader<T> {
+	data: T[] = [];
+
+	constructor(public filename: string) {}
+
+	read(): void {
+		this.data = fs
+			.readFileSync(this.filename, {
+				encoding: 'utf-8'
+			})
+			.split('\n')
+			.map((row: string): string[] => row.split(','))
+			.map(this.mapRow);
+	}
+
+	abstract mapRow(row: string[]): T;
+}
