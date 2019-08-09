c76b8f5cd9b1fd1c1068fdeae2a5aa9920799e68
Alan Maldonado <alan.maldonado@scalablepress.com>
2019-08-09 16:14:08 -0500

    ✨ Console Reporter

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,22 +1,19 @@
 // import { MatchReader } from './inheritance/MatchReader';
 import { CsvFileReader } from './composition/CsvFileReader';
 import { MatchReader } from './composition/MatchReader';
-import { MatchResult } from './MatchResult';
+import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
+import { Summary } from './Summary';
 
 const csvFileReader = new CsvFileReader('football.csv');
 
 // const reader = new MatchReader('football.csv');
-const reader = new MatchReader(csvFileReader);
-reader.load();
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
-let manUnitedWins = 0;
+const summary = new Summary(
+  new WinsAnalysis('Man United'),
+  new ConsoleReport()
+);
 
-for (let match of reader.matches) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-
-console.log(`Man United won ${manUnitedWins} games`);
+summary.buildAndPrintReport(matchReader.matches);
