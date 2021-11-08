f28cdb37801dd64ac6162f0cd0d8af512669f92b
Alan Maldonado <alan.maldonado@gmail.com>
2021-11-08 14:56:53 -0600

    ✨ Nested components

--- web/src/views/View.ts
+++ web/src/views/View.ts
@@ -4,8 +4,17 @@ export abstract class View<T extends Model<K>, K> {
   constructor(public parent: Element, public model: T) {
     this.bindModel();
   }
-  abstract eventsMap(): { [key: string]: () => void };
   abstract template(): string;
+
+  regions: { [key: string]: Element } = {};
+
+  regionsMap(): { [key: string]: string } {
+    return {};
+  }
+
+  eventsMap(): { [key: string]: () => void } {
+    return {};
+  }
 
   bindModel(): void {
     this.model.on('change', () => {
@@ -25,12 +34,29 @@ export abstract class View<T extends Model<K>, K> {
     }
   }
 
+  mapRegions(fragment: DocumentFragment): void {
+    const regionsMap = this.regionsMap();
+
+    for (let key in regionsMap) {
+      const selector = regionsMap[key];
+      const element = fragment.querySelector(selector);
+      if (element) {
+        this.regions[key] = element;
+      }
+    }
+  }
+
+  onRender(): void {}
+
   render(): void {
     this.parent.innerHTML = '';
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
 
     this.bindEvents(templateElement.content);
+    this.mapRegions(templateElement.content);
+
+    this.onRender();
 
     this.parent.append(templateElement.content);
   }
